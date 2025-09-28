7a708b0c4d88fbebc3e78df0205acc1f65e1000f
thatguyErik <eriksamuelbien@gmail.com>
2025-09-28 14:30:17 +0800

    Erik : Added my Signup, Help , Profile and edit profile , and added css base style and for authentication

--- accounts/models.py
+++ accounts/models.py
@@ -2,9 +2,9 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 class CustomUser(AbstractUser):
-    ROLE_CHOICES = (
-        ('student', 'Student'),
-        ('teacher', 'Teacher'),
-        ('admin', 'Admin'),
+    role = models.CharField(
+        max_length=10,
+        choices=[("student", "Student"), ("teacher", "Teacher"), ("admin", "Admin")],
+        default="student"
     )
-    role = models.CharField(max_length=10, choices=ROLE_CHOICES, default="student")
+    student_id = models.CharField(max_length=20, blank=True, null=True)
--- collaborative_whiteboard_backend/settings.py
+++ collaborative_whiteboard_backend/settings.py
@@ -130,6 +130,8 @@ LOGOUT_REDIRECT_URL = "/accounts/login/"
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
 #STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / "media"
